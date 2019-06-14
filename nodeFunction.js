76d8347b19abf7aa9b4b2c2fe2d1e09f70d09259
DalitsoKasonde <dkasonde11@gmail.com>
2019-06-14 18:48:46 +0200

    add function and screenshoot

---
+++ nodeFunction.js
@@ -0,0 +1,12 @@
+const name = "Dalitso"
+const otherName = "Bernadette"
+const earthBornDate = 100000000
+const yearToDay = 2019 
+
+function learn () {
+    let earthAge = earthBornDate - yearToDay
+    return "Am " + name + " " + otherName + " and I live on earth which I think is " + earthAge
+
+}
+
+console.log(learn());
---
+++ nodeTest.js
@@ -0,0 +1,7 @@
+const name = "Dalitso"
+  const otherName = "Bernadette"
+  const earthBornDate = 100000000
+  const yearToDay = 2019
+  let earthAge = earthBornDate - yearToDay 
+  console.log("Am " + name + " " + otherName + " and I live on earth which I think is "+earthAge)
+  
